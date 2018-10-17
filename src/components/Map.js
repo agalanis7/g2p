948579623e2a7aaac6fcaf1c7eb7324081870777
Karina Pietra <karinapietra95@gmail.com>
2018-10-17 15:33:40 -0400

    React env var is silly

--- src/components/Map.js
+++ src/components/Map.js
@@ -17,7 +17,7 @@ class Map extends Component {
  }
 
   async componentDidMount() {
-    mapboxgl.accessToken = process.env.API_KEY
+    mapboxgl.accessToken = process.env.REACT_APP_API_KEY
     const mapOptions = {
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v9',
